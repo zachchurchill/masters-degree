ffc995e49d6cae8498a8a3dfa76620fa0f0a01d0
Zach Churchill <zachurchill@protonmail.com>
2024-04-29 22:10:02 -0400

    finish homework 10

--- COMP511/src/main/java/com/zachurchill/homework10/TrainArranger.java
+++ COMP511/src/main/java/com/zachurchill/homework10/TrainArranger.java
@@ -2,8 +2,16 @@ package com.zachurchill.homework10;
 
 public class TrainArranger {
     public void arrange(TrainYard yard, Car [] ordering) {
-        while (yard.frontOf(Track.INPUT) != null) {
-            yard.move(Track.INPUT, Track.OUTPUT);
+        int nextCarIndex = 0;
+        while (nextCarIndex < ordering.length) {
+            Track from = yard.containsCar(Track.INPUT, ordering[nextCarIndex]) ? Track.INPUT : Track.SIDING;
+            Track holding = from == Track.INPUT ? Track.SIDING : Track.INPUT;
+            if (yard.frontOf(from).equals(ordering[nextCarIndex])) {
+                yard.move(from, Track.OUTPUT);
+                ++nextCarIndex;
+            } else {
+                yard.move(from, holding);
+            }
         }
         System.out.println(yard.toString());
     }
--- COMP511/src/test/java/com/zachurchill/homework10/TrainArrangerTest.java
+++ COMP511/src/test/java/com/zachurchill/homework10/TrainArrangerTest.java
@@ -26,7 +26,7 @@ class TrainArrangerTest {
         assertNotNull(yard.frontOf(Track.OUTPUT));
         Rail outputRail = yard.getOutputRail();
         for (int i = expectedOrdering.length - 1; i >= 0; --i) {
-            // stacks are last-in-first-out so we need to check in reverse
+            // stacks are LIFO so we need to check in reverse
             assertEquals(expectedOrdering[i], outputRail.pop());
         }
     }
@@ -91,11 +91,55 @@ class TrainArrangerTest {
 
     @Test
     void test10Cars() {
-        fail("Write your test case here.");
+        TrainYard yard;
+        int [] cars1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int [] cars2 = {4, 5, 6, 1, 2, 3, 10, 9, 8, 7};
+        Car [] expectedOrdering1 = {new Car(1), new Car(2), new Car(10), new Car(9), new Car(3), new Car(4), new Car(8), new Car(7), new Car(5), new Car(6)};
+        Car [] expectedOrdering2 = {new Car(10), new Car(1), new Car(9), new Car(2), new Car(8), new Car(3), new Car(7), new Car(4), new Car(6), new Car(5)};
+
+        // INPUT {1, ..., 10}; OUTPUT {Car(1), Car(2), Car(10), Car(9), Car(3), Car(4), Car(8), Car(7), Car(5), Car(6)};
+        yard = new TrainYard(cars1);
+        checkInitialState(yard);
+        this.trainArranger.arrange(yard, expectedOrdering1);
+        checkArrangedYard(yard, expectedOrdering1);
+
+        // INPUT {1, ..., 10}; OUTPUT {Car(10), Car(1), Car(9), Car(2), Car(8), Car(3), Car(7), Car(4), Car(6), Car(5)};
+        yard = new TrainYard(cars1);
+        checkInitialState(yard);
+        this.trainArranger.arrange(yard, expectedOrdering2);
+        checkArrangedYard(yard, expectedOrdering2);
+
+        // INPUT {4, 5, 6, 1, 2, 3, 10, 9, 8, 7}; OUTPUT {Car(1), Car(2), Car(10), Car(9), Car(3), Car(4), Car(8), Car(7), Car(5), Car(6)};
+        yard = new TrainYard(cars2);
+        checkInitialState(yard);
+        this.trainArranger.arrange(yard, expectedOrdering1);
+        checkArrangedYard(yard, expectedOrdering1);
+
+        // INPUT {4, 5, 6, 1, 2, 3, 10, 9, 8, 7}; OUTPUT {Car(10), Car(1), Car(9), Car(2), Car(8), Car(3), Car(7), Car(4), Car(6), Car(5)};
+        yard = new TrainYard(cars2);
+        checkInitialState(yard);
+        this.trainArranger.arrange(yard, expectedOrdering2);
+        checkArrangedYard(yard, expectedOrdering2);
     }
 
     @Test
     void test20Cars() {
-        fail("Write your test case here.");
+        TrainYard yard;
+        int [] cars1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        int [] cars2 = {4, 5, 6, 1, 2, 3, 10, 9, 8, 7, 19, 18, 12, 13, 11, 14, 17, 20, 15, 16};
+        Car [] expectedOrdering = {
+            new Car(1), new Car(2), new Car(10), new Car(9), new Car(3), new Car(4), new Car(8), new Car(7), new Car(5), new Car(6),
+            new Car(20), new Car(11), new Car(13), new Car(12), new Car(19), new Car(17), new Car(18), new Car(15), new Car(14), new Car(16)
+        };
+
+        yard = new TrainYard(cars1);
+        checkInitialState(yard);
+        this.trainArranger.arrange(yard, expectedOrdering);
+        checkArrangedYard(yard, expectedOrdering);
+
+        yard = new TrainYard(cars2);
+        checkInitialState(yard);
+        this.trainArranger.arrange(yard, expectedOrdering);
+        checkArrangedYard(yard, expectedOrdering);
     }
 }
