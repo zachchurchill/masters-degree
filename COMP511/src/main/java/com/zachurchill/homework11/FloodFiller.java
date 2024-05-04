2382b34e8df60dfff764b61b616589987c3e2486
Zach Churchill <zachurchill@protonmail.com>
2024-05-03 22:18:29 -0400

    add very poor fill algorithm for test cases

--- COMP511/src/main/java/com/zachurchill/homework11/FloodFiller.java
+++ COMP511/src/main/java/com/zachurchill/homework11/FloodFiller.java
@@ -12,7 +12,100 @@ public class FloodFiller {
     }
 
     public void fill(Point p, char substitute) {
-        throw new UnsupportedOperationException("Your code here.");
+        if (p.x < 0 || p.x >= this.image.length || p.y < 0 || p.y >= this.image[0].length) {
+            throw new IllegalArgumentException();
+        }
+
+        boolean keepFilling = true;
+        for (int j = p.y; j < this.image[p.x].length; ++j) {
+            if (this.image[p.x][j] != '.' && keepFilling) {
+                this.image[p.x][j] = substitute;
+            } else if (keepFilling) {
+                keepFilling = false;
+            }
+        }
+
+        for (int i = 0; i < p.x; ++i) {
+            for (int j = 0; j < this.image[i].length; ++j) {
+                if (this.image[i][j] == '.') {
+                    continue;
+                } else if (
+                    didBelowChange(new Point(i, j), substitute) ||
+                    didAboveChange(new Point(i, j), substitute) ||
+                    didLeftChange(new Point(i, j), substitute) ||
+                    didRightChange(new Point(i, j), substitute)
+                ) {
+                    this.image[i][j] = substitute;
+                }
+            }
+            for (int j = this.image[i].length - 1; j >= 0 ; --j) {
+                if (this.image[i][j] == '.') {
+                    continue;
+                } else if (
+                    didBelowChange(new Point(i, j), substitute) ||
+                    didAboveChange(new Point(i, j), substitute) ||
+                    didLeftChange(new Point(i, j), substitute) ||
+                    didRightChange(new Point(i, j), substitute)
+                ) {
+                    this.image[i][j] = substitute;
+                }
+            }
+        }
+
+        for (int i = p.x + 1; i < this.image.length; ++i) {
+            for (int j = 0; j < this.image[i].length; ++j) {
+                if (this.image[i][j] == '.') {
+                    continue;
+                } else if (
+                    didBelowChange(new Point(i, j), substitute) ||
+                    didAboveChange(new Point(i, j), substitute) ||
+                    didLeftChange(new Point(i, j), substitute) ||
+                    didRightChange(new Point(i, j), substitute)
+                ) {
+                    this.image[i][j] = substitute;
+                }
+            }
+            for (int j = this.image[i].length - 1; j >= 0 ; --j) {
+                if (this.image[i][j] == '.') {
+                    continue;
+                } else if (
+                    didBelowChange(new Point(i, j), substitute) ||
+                    didAboveChange(new Point(i, j), substitute) ||
+                    didLeftChange(new Point(i, j), substitute) ||
+                    didRightChange(new Point(i, j), substitute)
+                ) {
+                    this.image[i][j] = substitute;
+                }
+            }
+        }
+    }
+
+    private boolean didLeftChange(Point p, char substitute) {
+        if (p.y - 1 < 0) {
+            return false;
+        }
+        return this.image[p.x][p.y - 1] == substitute;
+    }
+
+    private boolean didRightChange(Point p, char substitute) {
+        if (p.y + 1 >= this.image[0].length) {
+            return false;
+        }
+        return this.image[p.x][p.y + 1] == substitute;
+    }
+
+    private boolean didAboveChange(Point p, char substitute) {
+        if (p.x - 1 < 0) {
+            return false;
+        }
+        return this.image[p.x - 1][p.y] == substitute;
+    }
+
+    private boolean didBelowChange(Point p, char substitute) {
+        if (p.x + 1 >= this.image.length) {
+            return false;
+        }
+        return this.image[p.x + 1][p.y] == substitute;
     }
 
     @Override
--- COMP511/src/test/java/com/zachurchill/homework11/MyTests.java
+++ COMP511/src/test/java/com/zachurchill/homework11/MyTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class MyTests {
-    private QueueImpl<Integer> queue;
+    private QueueImpl<Character> queue;
 
     @BeforeEach
     public void setUp() {
@@ -17,19 +17,66 @@ class MyTests {
     @Test
     void testQueueInterfaceMethods() {
         assertTrue(queue.isEmpty());
-        queue.enqueue(10);
+        queue.enqueue('X');
         assertFalse(queue.isEmpty());
-        assertEquals(10, queue.dequeue());
+        assertEquals('X', queue.dequeue());
         assertTrue(queue.isEmpty());
-        queue.enqueue(10);
-        queue.enqueue(20);
-        assertEquals(10, queue.dequeue());
-        assertEquals(20, queue.dequeue());
+        queue.enqueue('X');
+        queue.enqueue('Y');
+        assertEquals('X', queue.dequeue());
+        assertEquals('Y', queue.dequeue());
         assertTrue(queue.isEmpty());
     }
 
     @Test
     void testQueueImplThrowsEmptyStackExceptionEmptyDequeue() {
         assertThrows(IndexOutOfBoundsException.class, () -> queue.dequeue());
+    }
+
+    @Test
+    void testBadStartingPoint() {
+        char [] firstRow = {'.', 'X', 'X', 'X', '.'};
+        char [] secondRow = {'.', '.', 'X', '.', '.'};
+        char [] thirdRow = {'.', 'X', 'X', 'X', '.'};
+        char [][] picture = {firstRow, secondRow, thirdRow};
+        FloodFiller filler = new FloodFiller(picture);
+        assertThrows(IllegalArgumentException.class, () -> filler.fill(new Point(-1, 0), 'Y'));
+        assertThrows(IllegalArgumentException.class, () -> filler.fill(new Point(1, -1), 'Y'));
+        assertThrows(IllegalArgumentException.class, () -> filler.fill(new Point(4, 1), 'Y'));
+        assertThrows(IllegalArgumentException.class, () -> filler.fill(new Point(1, 5), 'Y'));
+    }
+
+    @Test
+    void testFillingQueueWithPicture() {
+        char [] firstRow = {'.', 'X', 'X', 'X', '.'};
+        char [] secondRow = {'.', '.', 'X', '.', '.'};
+        char [] thirdRow = {'.', 'X', 'X', 'X', '.'};
+        char [][] picture = {firstRow, secondRow, thirdRow};
+        FloodFiller filler = new FloodFiller(picture);
+        System.out.println(filler.toString());
+        filler.fill(new Point(1, 2), 'Y');
+        System.out.println(filler.toString());
+        char [][] filledPicture = filler.getRawImage();
+        assertEquals(3, filledPicture.length);
+        assertArrayEquals(new char[] {'.', 'Y', 'Y', 'Y', '.'}, filledPicture[0]);
+        assertArrayEquals(new char[] {'.', '.', 'Y', '.', '.'}, filledPicture[1]);
+        assertArrayEquals(new char[] {'.', 'Y', 'Y', 'Y', '.'}, filledPicture[2]);
+    }
+
+    @Test
+    void testProvidedExample() {
+        char [] firstRow = {'X', 'X', 'X', 'X', '.', '.', '.', '.', 'X', 'X', 'X', 'X'};
+        char [] secondRow = {'X', 'X', '.', '.', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'};
+        char [] thirdRow = {'.', 'X', 'X', 'X', '.', '.', '.', 'X', 'X', 'X', 'X', 'X'};
+        char [][] picture = {firstRow, secondRow, thirdRow};
+        FloodFiller filler = new FloodFiller(picture);
+        System.out.println(filler.toString());
+        filler.fill(new Point(1, 4), 'Y');
+        System.out.println(filler.toString());
+        char [][] filledPicture = filler.getRawImage();
+        assertEquals(3, filledPicture.length);
+        assertArrayEquals(new char[] {'X', 'X', 'X', 'X', '.', '.', '.', '.', 'Y', 'Y', 'Y', 'Y'}, filledPicture[0]);
+        assertArrayEquals(new char[] {'X', 'X', '.', '.', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y'}, filledPicture[1]);
+        assertArrayEquals(new char[] {'.', 'X', 'X', 'X', '.', '.', '.', 'Y', 'Y', 'Y', 'Y', 'Y'}, filledPicture[2]);
     }
 }
