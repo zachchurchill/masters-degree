cdda5c935bf28d76f368339a1747cf90a00cf399
Zach Churchill <zachurchill@protonmail.com>
2024-06-08 23:30:58 -0400

    Add non-recursive solution for string generator

--- COMP611/src/main/java/com/zachurchill/homework3/StringGenerator.java
+++ COMP611/src/main/java/com/zachurchill/homework3/StringGenerator.java
@@ -5,7 +5,26 @@ import java.util.List;
 
 public class StringGenerator {
     public List<String> generateAllStrings(int length, int base) {
-        return null;
+        ArrayList<String> result = new ArrayList<>();
+        // length = 2; base = 2 => { 00, 01, 10, 11 }
+        // length = 3; base = 5 => { 000, 001, 002, 003, 004, ..., 440, 441, 442, 443, 444 }
+        // what's the base case here?
+        // can I do this without recursion first.
+        for (int b = 0; b < base; b++) {
+            result.add(String.valueOf(b));
+        }
+        ArrayList<String> temp = new ArrayList<>();
+        for (int i = 1; i < length; i++) {
+            for (int b = 0; b < base; b++)  {
+                for (String r : result) {
+                    temp.add(String.format("%d%s", b, r));
+                }
+            }
+            result.clear();
+            result.addAll(temp);
+            temp.clear();
+        }
+        return result;
     }
 
     public static void main(String[] args) {
--- COMP611/src/test/java/com/zachurchill/homework3/StringGeneratorTests.java
+++ COMP611/src/test/java/com/zachurchill/homework3/StringGeneratorTests.java
@@ -2,6 +2,8 @@ package com.zachurchill.homework3;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +17,40 @@ class StringGeneratorTests {
   }
 
   @Test
-  public void testSomething() {
-    fail("TODO");
+  public void testBase2Length3() {
+    ArrayList<String> expected = new ArrayList<>();
+    expected.add("000");
+    expected.add("010");
+    expected.add("001");
+    expected.add("100");
+    expected.add("110");
+    expected.add("101");
+    expected.add("011");
+    expected.add("111");
+    assertTrue(expected.containsAll(this.strings.generateAllStrings(3, 2)));
+    assertTrue(this.strings.generateAllStrings(3, 2).containsAll(expected));
+  }
+
+  @Test
+  public void testBase4Length2() {
+    ArrayList<String> expected = new ArrayList<>();
+    expected.add("00");
+    expected.add("01");
+    expected.add("02");
+    expected.add("03");
+    expected.add("10");
+    expected.add("11");
+    expected.add("12");
+    expected.add("13");
+    expected.add("20");
+    expected.add("21");
+    expected.add("22");
+    expected.add("23");
+    expected.add("30");
+    expected.add("31");
+    expected.add("32");
+    expected.add("33");
+    assertTrue(expected.containsAll(this.strings.generateAllStrings(2, 4)));
+    assertTrue(this.strings.generateAllStrings(2, 4).containsAll(expected));
   }
 }
